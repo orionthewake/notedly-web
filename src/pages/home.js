bd07f80083025a0b83b23487761d9a1467a8f82e
Joseph Howard <joe.howard@mac.com>
2023-02-22 14:54:15 -0500

    Create styled Button

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import Button from '../components/Button';
 
 const Home = () => {
+  useEffect(() => {
+    // update the document title
+    document.title = 'Notedly';
+  });
+
   return (
     <div>
       <p>This is the home page</p>
+      <Button>Click me</Button>
     </div>
   );
 };
