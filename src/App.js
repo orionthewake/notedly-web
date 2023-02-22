71033fd1b94c890ebeb55cbf7e2dde81c1bd0d03
Joseph Howard <joe.howard@mac.com>
2023-02-22 12:38:22 -0500

    Basic App.js

--- src/App.js
+++ src/App.js
@@ -1,2 +1,13 @@
-// index.js
-// This is the main entry point of our application
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hi Notedly!</h1>
+      <p>Welcome to the Notedly application</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
