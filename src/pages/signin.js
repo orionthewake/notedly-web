32e0d51ddb3d929524099e3762e8eebb452869f1
Joseph Howard <joe.howard@mac.com>
2023-02-23 13:31:28 -0500

    Extract UserForm from SignUp and add SignIn form

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,15 +1,44 @@
 import React, { useEffect } from 'react';
+import { useMutation, useApolloClient, gql } from '@apollo/client';
 
-const SignIn = () => {
+import UserForm from '../components/UserForm';
+
+const SIGNIN_USER = gql`
+  mutation signIn($email: String!, $password: String!) {
+    signIn(email: $email, password: $password)
+  }
+`;
+
+const SignIn = props => {
   useEffect(() => {
     // update the document title
-    document.title = 'Sign In - Notedly';
+    document.title = 'Sign In — Notedly';
+  });
+
+  const client = useApolloClient();
+
+  const [signIn, { loading, error }] = useMutation(SIGNIN_USER, {
+    onCompleted: data => {
+      // store the token
+      localStorage.setItem('token', data.signIn);
+      // update the local cache
+      client.writeData({ data: { isLoggedIn: true } });
+      // redirect the user to the homepage
+      props.history.push('/');
+    },
+    onError: error => {
+      console.log(error);
+    },
   });
 
   return (
-    <div>
-      <p>Sign In</p>
-    </div>
+    <React.Fragment>
+      <UserForm action={signIn} formType="signin" />
+      {/* if the data is loading, display a loading message*/}
+      {loading && <p>Loading...</p>}
+      {/* if there is an error, display a error message*/}
+      {error && <p>Error signing in!</p>}
+    </React.Fragment>
   );
 };
 
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import Button from '../components/Button';
 import { useMutation, useApolloClient, gql } from '@apollo/client';
+
+import UserForm from '../components/UserForm';
 
 const SIGNUP_USER = gql`
   mutation signUp($email: String!, $username: String!, $password: String!) {
@@ -9,42 +9,13 @@ const SIGNUP_USER = gql`
   }
 `;
 
-const Wrapper = styled.div`
-  border: 1px solid #f5f4f0;
-  max-width: 500px;
-  padding: 1em;
-  margin: 0 auto;
-`;
-
-const Form = styled.form`
-  label,
-  input {
-    display: block;
-    line-height: 2em;
-  }
-  input {
-    width: 100%;
-    margin-bottom: 1em;
-  }
-`;
-
 const SignUp = props => {
-  const [values, setValues] = useState();
-
-  const onChange = event => {
-    setValues({
-      ...values,
-      [event.target.name]: event.target.value,
-    });
-  };
-
   useEffect(() => {
     // update the document title
-    document.title = 'Sign Up - Notedly';
+    document.title = 'Sign Up — Notedly';
   });
 
   const client = useApolloClient();
-
   const [signUp, { loading, error }] = useMutation(SIGNUP_USER, {
     onCompleted: data => {
       // store the token
@@ -57,48 +28,13 @@ const SignUp = props => {
   });
 
   return (
-    <Wrapper>
-      <h2>Sign Up</h2>
-      <Form
-        onSubmit={event => {
-          event.preventDefault();
-          signUp({
-            variables: {
-              ...values,
-            },
-          });
-        }}
-      >
-        <label htmlFor="username">Username:</label>
-        <input
-          required
-          type="text"
-          id="username"
-          name="username"
-          placeholder="username"
-          onChange={onChange}
-        />
-        <label htmlFor="email">Email:</label>
-        <input
-          required
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Email"
-          onChange={onChange}
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          required
-          type="password"
-          id="password"
-          name="password"
-          placeholder="Password"
-          onChange={onChange}
-        />
-        <Button type="submit">Submit</Button>
-      </Form>
-    </Wrapper>
+    <React.Fragment>
+      <UserForm action={signUp} formType="signup" />
+      {/* if the data is loading, display a loading message*/}
+      {loading && <p>Loading...</p>}
+      {/* if there is an error, display a error message*/}
+      {error && <p>Error creating an account!</p>}
+    </React.Fragment>
   );
 };
 
